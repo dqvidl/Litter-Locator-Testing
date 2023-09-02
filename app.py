13aec8d4f9463023e34afa30087bf6fb039c11bd
david liang <96744843+dqvidl@users.noreply.github.com>
2023-09-02 04:56:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,30 @@
 import streamlit as st
-import pandas as pd
+from geopy.geocoders import Nominatim
 
-# Create a DataFrame to store reported litter locations
-reported_locations = pd.DataFrame(columns=["Location Name", "Description", "Latitude", "Longitude"])
+# Set the title of the web app
+st.title("Address to Coordinates Converter")
 
-# Create a Streamlit sidebar for reporting litter
-st.sidebar.title("Report Litter")
-location_name = st.sidebar.text_input("Location Name")
-description = st.sidebar.text_area("Description")
-latitude = st.sidebar.number_input("Latitude", min_value=-90.0, max_value=90.0)
-longitude = st.sidebar.number_input("Longitude", min_value=-180.0, max_value=180.0)
+# Create a text input for the user to enter an address
+user_address = st.text_input("Enter an address:")
 
-if st.sidebar.button("Submit Report"):
-    if location_name and description and latitude and longitude:
-        reported_locations = reported_locations.append(
-            {"Location Name": location_name, "Description": description, "Latitude": latitude, "Longitude": longitude},
-            ignore_index=True,
-        )
-        st.sidebar.success("Litter location reported successfully!")
+# Create a button to trigger the conversion
+if st.button("Convert"):
+    if user_address:
+        # Initialize a geocoder
+        geolocator = Nominatim(user_agent="address-to-coordinates-converter")
 
-# Create a Streamlit main content for locating and cleaning litter
-st.title("Litter Locator")
-st.markdown(
-    "Welcome to Litter Locator! You can report litter locations on the left sidebar, and cleaning teams can use this page to find and clean these places."
-)
+        try:
+            # Attempt to geocode the user's input address
+            location = geolocator.geocode(user_address)
 
-# Display a map of reported litter locations
-st.map(reported_locations)
+            if location:
+                # Display the latitude and longitude
+                st.success(f"Latitude: {location.latitude}, Longitude: {location.longitude}")
+            else:
+                st.error("Address not found. Please enter a valid address.")
 
-# Create a search form for cleaning teams
-st.header("Search Litter Locations")
-search_latitude = st.number_input("Search Latitude", min_value=-90.0, max_value=90.0)
-search_longitude = st.number_input("Search Longitude", min_value=-180.0, max_value=180.0)
-search_radius = st.slider("Search Radius (miles)", min_value=1, max_value=100, step=1, value=10)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
-if st.button("Search"):
-    # Filter reported locations within the search radius
-    filtered_locations = reported_locations.copy()
-    filtered_locations["Distance"] = filtered_locations.apply(
-        lambda row: (
-            (search_latitude - row["Latitude"]) ** 2 + (search_longitude - row["Longitude"]) ** 2
-        ) ** 0.5,
-        axis=1,
-    )
-    filtered_locations = filtered_locations[filtered_locations["Distance"] <= (search_radius / 69)]  # Approximate miles to degrees conversion
-
-    # Display filtered locations
-    if not filtered_locations.empty:
-        st.success("Found litter locations within the search radius:")
-        st.map(filtered_locations)
-        st.table(filtered_locations.drop(columns="Distance"))
     else:
-        st.warning("No litter locations found within the search radius.")
-
-# Note: You can add more functionality like user authentication, data persistence, and more as needed.
-
-
-Note: You can further customize the layout and appearance of the app as needed.
+        st.warning("Please enter an address to convert.")
